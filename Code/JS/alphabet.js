251c6f04d5d69f2c6c9158299581d99c493931f6
Mobolaji Ajibola Jr <bolaex@yahoo.com>
2026-02-26 12:32:16 +0100

    Characer branch

--- Code/JS/alphabet.js
+++ Code/JS/alphabet.js
@@ -73,7 +73,7 @@ export function alphabetStats(object) {
       return combine + count;
     }, 0);
 
-    let percentage = (brace.count / totalCount)       * 100;
+    let percentage = (brace.count / totalCount) * 100;
 
     let letterStats = document.createElement("div");
     letterStats.className = "letter-stats";
@@ -90,7 +90,7 @@ export function alphabetStats(object) {
     progressBars.append(div, progress, letterStats);
 
     const displayGraph = () => {
-      let appended = wrapper.appendChild(progressBars);
+      let appended = wrapper.appendChild(progressBars) ? true : false;
       if (appended) {
         wrapper.style.height = "200px";
         statsParagraph.style.display = "none";
